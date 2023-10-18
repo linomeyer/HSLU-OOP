67692ad127a7e70b6ccf5aa31b2aa5dc4a2d88b1
Lino Meyer <lino.meyer@bluewin.ch>
2023-10-18 21:06:46 +0200

    add iterations exercise (sw03)

--- sw03/src/main/java/ch/hslu/oop/sw03/e01/Demo.java
+++ sw03/src/main/java/ch/hslu/oop/sw03/e01/Demo.java
@@ -2,6 +2,54 @@ package ch.hslu.oop.sw03.e01;
 
 public class Demo {
 
+    public void printBox(final int height, final int length) {
+        for (int i = 0; i < height; i++) {
+            if (i == 0 || i == height - 1) {
+                printFullRow(length);
+            } else {
+                for (int j = 1; j <= length; j++) {
+                    if (j == 1 || j == length) {
+                        System.out.print("#");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+            }
+
+            System.out.println();
+        }
+    }
+
+    private void printFullRow(final int length) {
+        for (int i = 0; i < length; i++) {
+            System.out.print("#");
+        }
+    }
+
+    public void print0To10() {
+        for (int i = 0; i <= 10; i++) {
+            System.out.print(i);
+        }
+    }
+
+    public void iterativeAdditionUntil1() {
+        float x = 0.9f;
+        int i = 0;
+        while (x < 1.0f) {
+            x += 0.000025f;
+            i++;
+            System.out.println(i + "   " + x);
+        }
+    }
+
+    public void iterativeAdditionUntil1WithForLoop() {
+        float x = 0.9f;
+        for (int i = 0; i < 4000; i++) {
+            x += 0.000025;
+            System.out.println(x);
+        }
+    }
+
     public int max1(int a, int b) {
         return Math.max(a, b);
     }
---
+++ sw03/src/main/java/ch/hslu/oop/sw03/e01/Main.java
@@ -0,0 +1,8 @@
+package ch.hslu.oop.sw03.e01;
+
+public class Main {
+    public static void main(String[] args) {
+        Demo demo = new Demo();
+        demo.printBox(8,16);
+    }
+}
